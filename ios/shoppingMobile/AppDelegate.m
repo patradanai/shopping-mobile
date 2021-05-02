fe22f4b5bb789e22123353c470e4be65383bde21
Patradanai <patradanai@Patradanais-MacBook-Pro.local>
2021-05-02 22:22:40 +0700

    update appdelegate.m

--- ios/shoppingMobile/AppDelegate.m
+++ ios/shoppingMobile/AppDelegate.m
@@ -11,6 +11,9 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -32,6 +35,10 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  
+#if RCT_DEV
+  [bridge moduleForClass:[RCTDevLoadingView class]];
+#endif
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"shoppingMobile"
                                             initialProperties:nil];
@@ -60,3 +67,4 @@ static void InitializeFlipper(UIApplication *application) {
 }
 
 @end
+

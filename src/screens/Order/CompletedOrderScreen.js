4b2deae03999d94f3f36d91bdc5a2bace8c75992
Patradanai <bloody_jojo@hotmail.com>
2021-05-05 17:32:31 +0700

    no order yet

--- src/screens/Order/CompletedOrderScreen.js
+++ src/screens/Order/CompletedOrderScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {View} from 'react-native';
 import {Text} from 'react-native-elements';
+import NoOrder from '../../components/NoOrder';
 
 const CompletedOrderScreen = props => {
+  if (!props.order?.length > 0) {
+    return <NoOrder />;
+  }
   return (
     <View>
       <Text>CompletedOrder</Text>
